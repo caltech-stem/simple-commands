8b557a1964ee4f8f811aa592dbfe03db9b120c93
Roman Tumaykin <roman@tumaykin.com>
2018-12-15 10:04:46 -0800

    initial code

---
+++ run.py
@@ -0,0 +1,21 @@
+from picar import back_wheels, front_wheels
+from time import sleep
+import picar
+
+picar.setup();
+
+fw = front_wheels.Front_Wheels(debug=False, db=db_file)
+bw = back_wheels.Back_Wheels(debug=False, db=db_file)
+
+bw.ready()
+fw.ready()
+
+SPEED = 60
+
+bw.ready();
+
+bw.speed = SPEED;
+
+bw.forward();
+sleep(1.00)
+bw.stop();
