9954b61759278bebd9ab9f31fc97af46cfa44ccd
TacoOtter123 <39072759+TacoOtter123@users.noreply.github.com>
2018-12-15 15:39:23 -0800

    My Branch

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from picar import back_wheels, front_wheels
 from time import sleep
 import picar
+import random
 
 picar.setup();
 
@@ -10,12 +11,15 @@ bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 bw.ready()
 fw.ready()
 
-SPEED = 60
+
 
 bw.ready();
 
-bw.speed = SPEED;
 
-bw.forward();
-sleep(1.00)
-bw.stop();
+
+for i in range (10):
+  SPEED = random.randint(10,100)
+  bw.speed = SPEED;
+  bw.forward();
+  sleep(random.randint(0,1,2)
+  bw.stop();
