6dfa64fd9f35aa6938a423e4848f71cb7b2efb34
Roman Tumaykin <roman@tumaykin.com>
2018-12-15 16:03:24 -0800

    Added db

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import picar
 
 picar.setup();
 
+db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
+
 fw = front_wheels.Front_Wheels(debug=False, db=db_file)
 bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 
