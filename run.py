56258d93db581525ac8c5ae64152cacfb4cb82d8
Ice Breakers Mint <lev@tumaykin.com>
2019-01-19 15:58:04 -0800

    my changes

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from picar import back_wheels, front_wheels
 from time import sleep
 import picar
-import random
 
 picar.setup();
 
@@ -13,9 +12,12 @@ bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 bw.ready()
 fw.ready()
 
-for i in range (10):
-  SPEED = random.randint(10,100)
-  bw.speed = SPEED;
+FSPEED = 100
+
+fw.turn_right();
+
+for i in range(5):
+  bw.speed = FSPEED;
   bw.forward();
-  sleep(random.randint(3))
-  bw.stop();
+sleep(1.00)
+bw.stop();
