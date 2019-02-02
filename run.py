b356e03d574d9ab38449b818c8603743b132f79f
Ryoya Watanabe <kiichiw@me.com>
2019-02-02 15:30:26 -0800

    no message

--- run.py
+++ run.py
@@ -10,12 +10,37 @@ bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 bw.ready()
 fw.ready()
 
-SPEED = 60
+SPEED = 70
 
 bw.ready();
+
+
 
 bw.speed = SPEED;
 
 bw.forward();
 sleep(2.00)
 bw.stop();
+bw.backward();
+sleep(2.00)
+bw.stop();
+sleep(2.00)
+bw.foward();
+fw.turn_right()
+sleep(2.00)
+bw.stop();
+fw.turn_straight()
+sleep(2.00)
+bw.backward();
+fw.turn_right
+sleep(2.00)
+bw.stop
+bw.forward();
+fw.turn_left()
+bw.stop();
+fw.turn_straight();
+sleep(2.00)
+
+
+
+
